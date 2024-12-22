5607a2a6933c89f42436a8dc4ea7cd144cbb8ec0
elfobella <yunusemreucr11@gmail.com>
2024-12-22 11:50:52 +0300

    selam

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Img1 from "./assets/1.jpg";
+
+function App() {
+  return (
+    <div>
+      <div>Aküler</div>
+      <div className="w-52">
+        <img src={Img1} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
