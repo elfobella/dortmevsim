e67225b28b5f8d51cb9c4777bbc48a3a9f65612e
elfobella <yunusemreucr11@gmail.com>
2024-12-22 11:57:25 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
-import Img1 from "./assets/1.jpg";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Products from "./Components/Products";
+import Reviews from "./Components/Reviews";
+import Slider from "./Components/Slider";
+import PropTypes from "prop-types";
+
+SectionWrapper.propTypes = {
+  children: PropTypes.node.isRequired, // 'children' türü doğrulandı
+};
+
+function SectionWrapper({ children }) {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Bileşen sadece bir kez animasyon uygular
+    threshold: 0.2, // Bileşenin %20'si görünür olduğunda tetikler
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }} // Animasyon öncesi durumu
+      animate={inView ? { opacity: 1, y: 0 } : {}} // Görünümdeyken animasyon
+      transition={{ duration: 0.5 }} // Animasyon süresi
+    >
+      {children}
+    </motion.div>
+  );
+}
 
 function App() {
+  const placeId = "ChIJpdjt4l5FyxQRr4TW6AOC3RU";
   return (
-    <div>
-      <div>Aküler</div>
-      <div className="w-52">
-        <img src={Img1} alt="" />
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Slider />
+      <SectionWrapper>
+        <Products />
+      </SectionWrapper>
+      <SectionWrapper>
+        <About />
+      </SectionWrapper>
+      <SectionWrapper>
+        <Contact />
+      </SectionWrapper>
+      <SectionWrapper>
+        <Reviews placeId={placeId} />
+      </SectionWrapper>
+      <Footer />
+    </>
   );
 }
 
